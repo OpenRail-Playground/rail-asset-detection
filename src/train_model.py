a7cfc84f1fdae96dec4f22542660f60e1a0f55e2
Daniel Ringler <daniel.ringler@deutschebahn.com>
2024-10-01 14:39:22 +0200

    add train_model script

---
+++ src/train_model.py
@@ -0,0 +1,26 @@
+"""Train model script.
+Copied from https://github.com/ultralytics/ultralytics
+"""
+
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolo11n.pt")
+
+# Train the model
+train_results = model.train(
+    data="coco8.yaml",  # path to dataset YAML
+    epochs=100,  # number of training epochs
+    imgsz=640,  # training image size
+    device="cpu",  # device to run on, i.e. device=0 or device=0,1,2,3 or device=cpu
+)
+
+# Evaluate model performance on the validation set
+metrics = model.val()
+
+# Perform object detection on an image
+results = model("path/to/image.jpg")
+results[0].show()
+
+# Export the model to ONNX format
+path = model.export(format="onnx")  # return path to exported model
